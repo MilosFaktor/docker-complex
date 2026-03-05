6117642cdfaa45df840809830227d00184efa314
Milos Faktor <faktor.milos@gmail.com>
2026-03-05 21:51:03 +0100

    introducing CI

--- commands.sh
+++ commands.sh
@@ -9,8 +9,11 @@ docker build -f Dockerfile.dev -t milosfaktor/server .
 ### worker
 docker build -f Dockerfile.dev -t milosfaktor/worker .
 
-### docker compose
+### docker compose up and build
 docker compose -f docker-compose-dev.yml up --build
+
+# docker compose down
+docker compose -f docker-compose-dev.yml down
 
 # removing volumes from docker compose
 docker compose -f docker-compose-dev.yml down -v
