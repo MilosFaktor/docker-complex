89e6332a2f72b1a45f5e769841cabadcf3783e9b
Milos Faktor <faktor.milos@gmail.com>
2026-03-23 19:09:31 +0100

    code update

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {});
+test("dummy test", () => {
+  expect(true).toBe(true);
+});
